4afc13b59e92c571185aa72ec260ecf9773b6ddc
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-02-01 22:00:08 -0700

    shows our final edits for the night

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,25 @@
-function nameMenuItem() {
-
+function nameMenuItem(Pizza) {
+return `Delicious ${Pizza}`
 }
+function createMenuItem() {
+return menuItem = {
+name: "Delicious French Toast",
+price: 10.99,
+type: "breakfast"
+}
+}
+function addIngredients(name) {
+ var ingredients = [] + ingredients.push(name)
+ return ingredients
+}
+  
+console.log(addIngredients('sausage'))
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
