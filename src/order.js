2b35bf1875139132703d05d8724d57a037fad8ee
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-02-03 10:27:47 -0700

    shows the order file down whaaaaaaaa

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+function takeOrder(order, deliveryOrders) {
+if (deliveryOrders.length < 3) {
+deliveryOrders.push(order)
+}
+return deliveryOrders
+}
+
+function refundOrder(orderToRefund, deliveryOrders) {
+  for (i = 0; i < deliveryOrders.length; i++) {
+if (deliveryOrders[i].orderNumber === orderToRefund) {
+  deliveryOrders.splice([i],1);
+}
+}
+return deliveryOrders
+}
+function listItems(deliveryOrders) {
+  
+  var namesArray = ""
+  for (i = 0; i < deliveryOrders.length-1; i++) {
+    namesArray += `${deliveryOrders[i].item}` + ", "
+  }
+  return namesArray += deliveryOrders[deliveryOrders.length - 1].item
+}
+
+function searchOrder(deliveryOrders, ordersItem) {
+  var newArrays = []
+  for (i = 0; i < deliveryOrders.length; i++) {
+    newArrays.push(deliveryOrders[i].item);
+  } 
+    return newArrays.includes(ordersItem)
+  }
+  
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   refundOrder,
+  listItems,
+   searchOrder
 }
